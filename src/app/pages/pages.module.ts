215b811846bfc0aed1e6042d5cb685c7e25292bb
Akash Bhardwaj <akash.bhardwaj@tavant.com>
2020-02-28 15:21:23 +0530

    added route for list component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,10 +5,12 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { AddNewComponent } from './add-new/add-new.component';
 
 
 @NgModule({
-  declarations: [PagesComponent, HomeComponent, ListComponent],
+  declarations: [PagesComponent, HomeComponent, ListComponent, DetailsComponent, AddNewComponent],
   imports: [
     CommonModule,
     PagesRoutingModule
